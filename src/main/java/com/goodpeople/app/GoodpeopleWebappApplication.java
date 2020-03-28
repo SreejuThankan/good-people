f231c67cee632648a5e594de9f80786244ef9f8a
Sreeju Thankan <coder.sthankan@gmail.com>
2020-03-28 20:07:05 +0000

    Initial project setup

---
+++ src/main/java/com/goodpeople/app/GoodpeopleWebappApplication.java
@@ -0,0 +1,13 @@
+package com.goodpeople.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodpeopleWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodpeopleWebappApplication.class, args);
+	}
+
+}
