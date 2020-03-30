8b36d677db49c5f6ca857ad3bedcb9f25561a10b
Sreeju Thankan <coder.sthankan@gmail.com>
2020-03-30 06:48:42 +0100

    Updating the scripts for deployment

--- deployment/scripts/install_front.sh
+++ deployment/scripts/install_front.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "Installing UI"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /var/opt/apps/good-people/front
 ls -rtl
 chmod 755 *
 ls -rtl
 npm install
 npm install @angular/cli
-ng build
+npm run-script ng build
 exit 0
 # change later
--- deployment/scripts/run_front.sh
+++ deployment/scripts/run_front.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Running the UI"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /var/opt/apps/good-people/front
 ls -rtl
-ng serve
+npm run-script ng serve
 exit 0
 # change later
