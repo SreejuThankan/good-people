ae4a000a239bf5497e618032571534d009365bff
Sreeju Thankan <coder.sthankan@gmail.com>
2020-03-30 08:34:08 +0100

    Updating the scripts for deployment

--- deployment/scripts/run_front.sh
+++ deployment/scripts/run_front.sh
@@ -10,7 +10,7 @@ npm install
 echo "Installing angular cli"
 npm install @angular/cli
 echo "Running ng build"
-ng build --prod=true --sourceMap=false 2> /dev/null
+ng build --prod=true --sourceMap=false 2> /var/opt/apps/good-people/logs/frontbuilderror.log
 echo "Running ng serve"
 ng serve > /var/opt/apps/good-people/logs/front.log 2> /var/opt/apps/good-people/logs/fronterror.log & echo $! > ../pids/front.pid
 sleep 30
