0a96d8889894bfad3d68ef1718af78e5c5c9e25b
Sreeju Thankan <coder.sthankan@gmail.com>
2020-03-30 08:31:16 +0100

    Updating the scripts for deployment

--- deployment/scripts/run_back.sh
+++ deployment/scripts/run_back.sh
@@ -3,5 +3,5 @@ echo "Running the service"
 export PATH="/home/ec2-user/.nvm/versions/node/v13.12.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin:$PATH"
 cd /var/opt/apps/good-people/back
 java -jar service.war > /var/opt/apps/good-people/logs/back.log 2> /var/opt/apps/good-people/logs/backerror.log & echo $! > ../pids/back.pid
-sleep 60
+sleep 30
 exit 0
